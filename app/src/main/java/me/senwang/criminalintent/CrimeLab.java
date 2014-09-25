cd161439d9e5dc1e342cbf71a64475091e33941d
Sen Wang <hitbear518@qq.com>
2014-09-25 15:22:55 +0800

    9/25 15:22 work, before c17

--- app/src/main/java/me/senwang/criminalintent/CrimeLab.java
+++ app/src/main/java/me/senwang/criminalintent/CrimeLab.java
@@ -1,11 +1,15 @@
 package me.senwang.criminalintent;
 
 import android.content.Context;
+import android.util.Log;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class CrimeLab {
+	private static final String TAG = CrimeLab.class.getSimpleName();
+	private static final String FILE_NAME = "crimes.json";
 
 	private static CrimeLab sCrimeLab;
 
@@ -18,10 +22,18 @@ public class CrimeLab {
 
 	private Context mAppContext;
 	private ArrayList<Crime> mCrimes;
+	private CriminalIntentJSONSerializer mSerializer;
 
 	private CrimeLab(Context appContext) {
 		mAppContext = appContext;
-		mCrimes = new ArrayList<Crime>();
+		mSerializer = new CriminalIntentJSONSerializer(appContext, FILE_NAME);
+
+		try {
+			mCrimes = mSerializer.loadCrimes();
+		} catch (Exception e) {
+			mCrimes = new ArrayList<Crime>();
+			Log.e(TAG, "Error loading crimes: ", e);
+		}
 	}
 
 	public ArrayList<Crime> getCrimes() {
@@ -37,5 +49,16 @@ public class CrimeLab {
 
 	public void addCrime(Crime c) {
 		mCrimes.add(c);
+	}
+
+	public boolean saveCrimes() {
+		try {
+			mSerializer.saveCrimes(mCrimes);
+			Log.d(TAG, "crimes saved to file");
+			return true;
+		} catch (Exception e) {
+			Log.e(TAG, "Error saving crimes: ", e);
+			return false;
+		}
 	}
 }
--- app/src/main/java/me/senwang/criminalintent/CriminalIntentJSONSerializer.java
+++ app/src/main/java/me/senwang/criminalintent/CriminalIntentJSONSerializer.java
@@ -3,9 +3,15 @@ package me.senwang.criminalintent;
 import android.content.Context;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -21,7 +27,7 @@ public class CriminalIntentJSONSerializer {
 		mFilename = filename;
 	}
 
-	public void saveCrimes(ArrayList<Crime> crimes) throws IOException {
+	public void saveCrimes(ArrayList<Crime> crimes) throws IOException, JSONException {
 		JSONArray array = new JSONArray();
 		for (Crime c : crimes) {
 			array.put(c.toJSON());
@@ -35,5 +41,29 @@ public class CriminalIntentJSONSerializer {
 		} finally {
 			if (writer != null) writer.close();
 		}
+	}
+
+	public ArrayList<Crime> loadCrimes() throws JSONException, IOException {
+		ArrayList<Crime> crimes = new ArrayList<Crime>();
+		BufferedReader reader = null;
+		try {
+			InputStream in = mContext.openFileInput(mFilename);
+			reader = new BufferedReader(new InputStreamReader(in));
+			StringBuilder jsonString = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				jsonString.append(line);
+			}
+			JSONArray array = (JSONArray) new JSONTokener(jsonString.toString()).nextValue();
+			for (int i = 0; i < array.length(); i++) {
+				JSONObject json = array.getJSONObject(i);
+				Crime crime = new Crime(json);
+				crimes.add(crime);
+			}
+		} catch (FileNotFoundException e) {
+		} finally {
+			if (reader != null) reader.close();
+		}
+		return crimes;
 	}
 }
