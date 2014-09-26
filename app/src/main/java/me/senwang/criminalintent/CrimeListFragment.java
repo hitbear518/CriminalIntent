684bce736b211f84e0c1b5c9fc6d03d57481071d
Sen Wang <hitbear518@qq.com>
2014-09-26 16:15:04 +0800

    9/26 16:14 work, before c18

--- app/src/main/java/me/senwang/criminalintent/CrimeListFragment.java
+++ app/src/main/java/me/senwang/criminalintent/CrimeListFragment.java
@@ -1,18 +1,24 @@
 package me.senwang.criminalintent;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.view.ActionMode;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ListView;
@@ -24,6 +30,7 @@ public class CrimeListFragment extends ListFragment {
 
 	private ArrayList<Crime> mCrimes;
 	private boolean mSubtitleVisible;
+	private ActionMode mActionMode;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -39,13 +46,65 @@ public class CrimeListFragment extends ListFragment {
 		setListAdapter(adapter);
 	}
 
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		ActionBar actionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
+		final ActionBar actionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
 		if (mSubtitleVisible) {
 			actionBar.setSubtitle(R.string.subtitle);
 		}
-		return super.onCreateView(inflater, container, savedInstanceState);
+
+		View v = super.onCreateView(inflater, container, savedInstanceState);
+		ListView listView = (ListView) v.findViewById(android.R.id.list);
+
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+			registerForContextMenu(listView);
+		} else {
+			listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+			listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+				@Override
+				public void onItemCheckedStateChanged(android.view.ActionMode mode, int position, long id, boolean checked) {
+				}
+
+				@Override
+				public boolean onCreateActionMode(android.view.ActionMode mode, Menu menu) {
+					mode.setTitle("Test Title");
+					MenuInflater inflater = mode.getMenuInflater();
+					inflater.inflate(R.menu.crime_list_item_context, menu);
+					return true;
+				}
+
+				@Override
+				public boolean onPrepareActionMode(android.view.ActionMode mode, Menu menu) {
+					return false;
+				}
+
+				@Override
+				public boolean onActionItemClicked(android.view.ActionMode mode, MenuItem item) {
+					switch (item.getItemId()) {
+						case R.id.menu_item_delete_crime:
+							CrimeAdapter adapter = (CrimeAdapter) getListAdapter();
+							CrimeLab crimeLab = CrimeLab.get(getActivity());
+							for (int i = adapter.getCount() - 1; i >= 0; i--) {
+								if (getListView().isItemChecked(i)) {
+									crimeLab.deleteCrime(adapter.getItem(i));
+								}
+							}
+							mode.finish();
+							adapter.notifyDataSetChanged();
+							return true;
+						default:
+							return false;
+					}
+				}
+
+				@Override
+				public void onDestroyActionMode(android.view.ActionMode mode) {
+				}
+			});
+		}
+
+		return v;
 	}
 
 	@Override
@@ -94,27 +153,49 @@ public class CrimeListFragment extends ListFragment {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.menu_item_new_crime:
-			Crime crime = new Crime();
-			CrimeLab.get(getActivity()).addCrime(crime);
-			Intent i = new Intent(getActivity(), CrimePagerActivity.class);
-			i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
-			startActivityForResult(i, 0);
-			return true;
-		case R.id.menu_item_show_subtitle:
-			ActionBar actionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
-			if (actionBar.getSubtitle() == null) {
-				actionBar.setSubtitle(R.string.subtitle);
-				mSubtitleVisible = true;
-				item.setTitle(R.string.hide_subtitle);
-			} else {
-				actionBar.setSubtitle(null);
-				mSubtitleVisible = false;
-				item.setTitle(R.string.show_subtitle);
-			}
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
+			case R.id.menu_item_new_crime:
+				Crime crime = new Crime();
+				CrimeLab.get(getActivity()).addCrime(crime);
+				Intent i = new Intent(getActivity(), CrimePagerActivity.class);
+				i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
+				startActivityForResult(i, 0);
+				return true;
+			case R.id.menu_item_show_subtitle:
+				ActionBar actionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
+				if (actionBar.getSubtitle() == null) {
+					actionBar.setSubtitle(R.string.subtitle);
+					mSubtitleVisible = true;
+					item.setTitle(R.string.hide_subtitle);
+				} else {
+					actionBar.setSubtitle(null);
+					mSubtitleVisible = false;
+					item.setTitle(R.string.show_subtitle);
+				}
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+		getActivity().getMenuInflater().inflate(R.menu.crime_list_item_context, menu);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+		int position = info.position;
+		CrimeAdapter adapter = (CrimeAdapter) getListAdapter();
+		Crime crime = adapter.getItem(position);
+
+		switch (item.getItemId()) {
+			case R.id.menu_item_delete_crime:
+				CrimeLab.get(getActivity()).deleteCrime(crime);
+				adapter.notifyDataSetChanged();
+				return true;
+			default:
+				return super.onContextItemSelected(item);
 		}
 	}
 
