66eb49800f2feb73aee80050ea960b5f8cba3aaa
WsAeNnG <hitbear518@qq.com>
2014-09-28 16:02:55 +0800

    928  zzz

---
+++ app/src/main/java/me/senwang/criminalintent/CrimeCameraActivity.java
@@ -0,0 +1,11 @@
+package me.senwang.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeCameraActivity extends SingleFragmentActivity{
+
+	@Override
+	protected Fragment createFragment() {
+		return new CrimeCameraFragment();
+	}
+}
