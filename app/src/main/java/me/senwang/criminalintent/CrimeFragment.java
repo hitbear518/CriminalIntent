d8de4412e0bb8fac18ae767492862408c510b6fc
WsAeNnG <hitbear518@qq.com>
2014-10-13 14:34:48 +0800

    10/13 14:34 work; before c20

--- app/src/main/java/me/senwang/criminalintent/CrimeFragment.java
+++ app/src/main/java/me/senwang/criminalintent/CrimeFragment.java
@@ -3,6 +3,7 @@ package me.senwang.criminalintent;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,7 +13,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +21,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.Date;
 import java.util.UUID;
@@ -31,6 +32,7 @@ public class CrimeFragment extends Fragment {
 	public static final String EXTRA_CRIME_ID = "me.senwang.criminalintent.CRIME_ID";
 
 	public static final String DIALOG_DATE = "date";
+	public static final String DIALOG_IMAGE = "image";
 
 	public static final int REQUEST_DATE = 0;
 	public static final int REQUEST_PHOTO = 1;
@@ -50,6 +52,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;
+	private ImageView mPhotoView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,6 +78,19 @@ public class CrimeFragment extends Fragment {
 			public void onClick(View v) {
 				Intent intent = new Intent(getActivity(), CrimeCameraActivity.class);
 				startActivityForResult(intent, REQUEST_PHOTO);
+			}
+		});
+
+		mPhotoView = (ImageView) v.findViewById(R.id.image_view);
+		mPhotoView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Photo p = mCrime.getPhoto();
+				if (p == null) return;
+
+				FragmentManager fm = getActivity().getSupportFragmentManager();
+				String path = getActivity().getFileStreamPath(p.getFilename()).getAbsolutePath();
+				ImageFragment.newInstance(path).show(fm, DIALOG_IMAGE);
 			}
 		});
 
@@ -135,9 +151,17 @@ public class CrimeFragment extends Fragment {
 		} else if (requestCode == REQUEST_PHOTO) {
 			String filename = data.getStringExtra(CrimeCameraFragment.EXTRA_PHOTO_FILENAME);
 			if (filename != null) {
-				Log.i(TAG, "filename: " + filename);
+				Photo p = new Photo(filename);
+				mCrime.setPhoto(p);
+				showPhoto();
 			}
 		}
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		showPhoto();
 	}
 
 	@Override
@@ -146,7 +170,24 @@ public class CrimeFragment extends Fragment {
 		CrimeLab.get(getActivity()).saveCrimes();
 	}
 
+	@Override
+	public void onStop() {
+		super.onStop();
+		PictureUtils.cleanImageView(mPhotoView);
+	}
+
 	private void updateDate() {
 		mDateButton.setText(mCrime.getDate().toString());
+	}
+
+	private void showPhoto() {
+		Photo p = mCrime.getPhoto();
+		BitmapDrawable b = null;
+		if (p != null) {
+			String path = getActivity()
+					.getFileStreamPath(p.getFilename()).getAbsolutePath();
+			b = PictureUtils.getScaledDrawable(getActivity(), path);
+		}
+		mPhotoView.setImageDrawable(b);
 	}
 }
---
+++ app/src/main/java/me/senwang/criminalintent/ImageFragment.java
@@ -0,0 +1,43 @@
+package me.senwang.criminalintent;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ImageFragment extends DialogFragment {
+	public static final String EXTRA_IMAGE_PATH = "me.senwang.criminalintent.image_path";
+
+	public static ImageFragment newInstance(String imagePath) {
+		Bundle args = new Bundle();
+		args.putString(EXTRA_IMAGE_PATH, imagePath);
+
+		ImageFragment fragment = new ImageFragment();
+		fragment.setArguments(args);
+		fragment.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+
+		return fragment;
+	}
+
+	private ImageView mImageView;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		mImageView = new ImageView(getActivity());
+		String path = getArguments().getString(EXTRA_IMAGE_PATH);
+		BitmapDrawable image = PictureUtils.getScaledDrawable(getActivity(), path);
+		mImageView.setImageDrawable(image);
+
+		return mImageView;
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		PictureUtils.cleanImageView(mImageView);
+	}
+}
